5a352a7ec92937b433652392997593dbe6c18352
guanshengheng <3406702953@qq.com>
2026-01-03 10:37:06 +0800

    删除

--- recruitment/c.cpp
+++ recruitment/c.cpp
@@ -2,58 +2,15 @@
 using namespace std;
 typedef long long ll;
 
-void solve()
-{
-    int q;
-    cin >> q;
-
-    priority_queue<int> l;
-
-    priority_queue<int, vector<int>, greater<int>> r;
-    while (q--)
-    {
-        char f;
-        cin >> f;
-
-        if (f == '+')
-        {
-            int x;
-            cin >> x;
-
-            if (l.empty() || x <= l.top())
-            {
-                l.push(x);
-            }
-            else
-            {
-                r.push(x);
-            }
-
-            if (l.size() > r.size() + 1)
-            {
-                r.push(l.top());
-                l.pop();
-            }
-            else if (r.size() > l.size())
-            {
-                l.push(r.top());
-                r.pop();
-            }
-        }
-        else if (f == '?')
-        {
-            cout << l.top() << "\n";
-        }
-    }
+void solve() {
+    
 }
 
-signed main()
-{
+signed main() {
     ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
     int t = 1;
-    // cin >> t;
-    while (t--)
-    {
+    cin >> t;
+    while (t--) {
         solve();
     }
     return 0;
